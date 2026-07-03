e79dfc7b1b7fc9e15592d9ef9a754decb6047308
AgusPod <103470074+AgusPod@users.noreply.github.com>
2026-07-03 14:24:09 -0300

    aWhatsApp: suma avisos de helada, granizo e inundacion al mensaje diario

--- scripts/enviar_whatsapp.py
+++ scripts/enviar_whatsapp.py
@@ -1,4 +1,5 @@
-"""Envia el mapa del dia por WhatsApp usando Twilio."""
+"""Envia el mapa del dia por WhatsApp usando Twilio, con avisos de
+helada, granizo e inundacion si los hay."""
 
 import json
 import os
@@ -11,12 +12,52 @@ def main():
     datos = json.loads(pathlib.Path("site/tiles.json").read_text())
     ndvi = datos.get("ndvi_medio_nacional")
 
-    texto = (
-        "Monitor Satelital Argentina - " + str(datos["fecha"]) + "\n"
-        + "NDVI medio nacional: " + str(ndvi) + "\n"
-        + "Compuesto de los ultimos " + str(datos["ventana_dias"]) + " dias.\n"
-        + "Dashboard: " + os.environ.get("SITE_URL", "")
-    )
+    lineas = [
+        "Monitor Satelital Argentina - " + str(datos["fecha"]),
+        "NDVI medio nacional: " + str(ndvi),
+    ]
+
+    heladas = datos.get("heladas")
+    if heladas:
+        frios = [d for d in heladas["departamentos"] if d["tmin_c"] <= 0]
+        riesgo = [d for d in heladas["departamentos"] if 0 < d["tmin_c"] <= 3]
+        if frios:
+            peor = frios[0]
+            lineas.append(
+                "HELADA (madrugada del " + heladas["fecha"] + "): "
+                + str(len(frios)) + " deptos bajo 0 C. Minima: "
+                + str(peor["tmin_c"]) + " C en " + peor["departamento"]
+                + " (" + peor["provincia"] + ")."
+            )
+        elif riesgo:
+            lineas.append(
+                "Riesgo de helada (madrugada del " + heladas["fecha"] + "): "
+                + str(len(riesgo)) + " deptos entre 0 y 3 C."
+            )
+
+    granizo = datos.get("granizo")
+    if granizo and granizo["departamentos"]:
+        peor = granizo["departamentos"][0]
+        lineas.append(
+            "POSIBLE GRANIZO ayer (" + granizo["fecha"] + "): "
+            + str(len(granizo["departamentos"])) + " deptos con conveccion severa. "
+            + "Nucleo mas frio: " + peor["departamento"] + " (" + peor["provincia"] + ")."
+        )
+
+    inund = datos.get("inundacion")
+    if inund and inund["departamentos"]:
+        peor = inund["departamentos"][0]
+        lineas.append(
+            "AGUA/INUNDACION: " + str(len(inund["departamentos"]))
+            + " deptos con senal. Mayor superficie: " + peor["departamento"]
+            + " (" + peor["provincia"] + "), " + str(peor["agua_nueva_ha"]) + " ha."
+        )
+
+    if len(lineas) == 2:
+        lineas.append("Sin alertas de helada, granizo ni inundacion.")
+
+    lineas.append("Dashboard: " + os.environ.get("SITE_URL", ""))
+    texto = "\n".join(lineas)
 
     client = Client(os.environ["TWILIO_SID"], os.environ["TWILIO_TOKEN"])
     msg = client.messages.create(
